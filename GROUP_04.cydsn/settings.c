cca447581e58fd0e53f4d0a0ca2b7ab185a7d792
giovanni6inve <giovanni6.invernizzi@mail.polimi.it>
2021-11-08 23:45:33 +0100

    add BCP cfg files

--- GROUP_04.cydsn/settings.c
+++ GROUP_04.cydsn/settings.c
@@ -9,11 +9,12 @@
  *
  * ========================================
 */
+#include <stdint.h>
 #include "settings.h"
 #include "project.h"
 #define offset_temp_mv 750 //mV, for temperature sensor calbration
 #define coef_temp 10 //mV/C, for temperature sensor calbration
-#define offset_temp_c 25 //C , for temperature sensor calbration
+#define offset_temp_c 25 //C , for temperature sensor calibration
 #define offset_lux 0.1//lux, for LDR calibration
 #define delta_lux 10000-0.1//lux for LDR calibration
 #define delta_ohm 100-1000000//ohm for LDR calibration
@@ -28,9 +29,13 @@ extern int32 value_digit[max_samples*2];//with 4 bits to do so 15 samples to ave
 uint8 flag_avgOVF;
 uint32 count_LEDovf=0;
 int32 sum=0, sum_0=0;
+//uint32_t* sum_temp = (int*)(&sum_0);
+//uint32_t* sum_ldr = (int*)(&sum);
+
 uint8 count=-1;
 
-
+               // Test value
+   
     /*this function takes in input the status: which channels to sample, and how many samples to average and 
     writes the registers of the slave buffer with the avg sample values and sets the timer and the ADC in 
     order to satisfy the requirements set by these parameters */
@@ -57,8 +62,8 @@ uint8 count=-1;
         if (count==Nsamples-1){ //once Nsamples for the ch0 are collected (starting count from 0 Nsamples are obtained at count=Nsamples-1)
             for (int i=0;i<Nsamples;i++){
                 
-                value_digit[i]=ADC_DelSig_CountsTo_mVolts(value_digit[i]);
-                value_digit[i]=(value_digit[i]-offset_temp_mv)/coef_temp + offset_temp_c;
+                //value_digit[i]=ADC_DelSig_CountsTo_mVolts(value_digit[i]);
+                //value_digit[i]=(value_digit[i]-offset_temp_mv)/coef_temp + offset_temp_c;
                 if (value_digit[i]>65535) value_digit[i]=65535;
                 if (value_digit[i]<0) value_digit[i]=0; //limit the dynamic to positive values within 65535 for robustness
                
@@ -78,8 +83,10 @@ uint8 count=-1;
         if (count==Nsamples*2-1){ //also the Nsamples for ch1 are collected (we have Nsamples*2 at the count Nsamples*2-1 since count start from 0)
             for (int j=Nsamples;j<Nsamples*2;j++){
                 
-                value_digit[j]=ADC_DelSig_CountsTo_Volts(value_digit[j]);
-                value_digit[j]= offset_lux + (delta_lux)*(R*(vcc-value_digit[j])/vcc*(1-(vcc-value_digit[j])/vcc)-offset_ohm)/delta_ohm ; 
+                //value_digit[j]=ADC_DelSig_CountsTo_Volts(value_digit[j]);
+                //value_digit[j]= offset_lux + (delta_lux)*(R*(vcc-value_digit[j])/vcc*(1-(vcc-value_digit[j])/vcc)-offset_ohm)/delta_ohm ; 
+                
+                
                 if (value_digit[j]>65535) value_digit[j]=65535;
                 if (value_digit[j]<0) value_digit[j]=0; //limit the dynamic to positive values within 65535 for robustness
                 
@@ -93,12 +100,12 @@ uint8 count=-1;
                 flag_avgOVF=1;  
             }
             
-            slaveBuffer[3]=(sum_0 & 0xFFFF) >>8; //since sum has 32 bits (output of read 32) we must extract here the MSB out of 2 bytes and right adjust it
-            slaveBuffer[4]= sum_0  & 0xFF;   //LSB 
+            slaveBuffer[3]=(sum & 0xFFFF) >>8; //since sum has 32 bits (output of read 32) we must extract here the MSB out of 2 bytes and right adjust it
+            slaveBuffer[4]= sum & 0xFF;   //LSB 
             sum_0=0; //reinitialize for the next 1/50Hz period
             
-            slaveBuffer[5]=(sum & 0xFFFF) >>8; 
-            slaveBuffer[6]= sum & 0xFF;            
+            slaveBuffer[5]=(sum_0 & 0xFFFF) >>8; 
+            slaveBuffer[6]= sum_0 & 0xFF;            
             sum=0; //reinitialize for the next 1/50Hz period
             count=-1; //reinitialize for the next 1/50Hz period
         }
@@ -106,7 +113,7 @@ uint8 count=-1;
     
     }
     
-    if ((flag_sample_ch0==1) &( flag_sample_ch1 == 0)){ //only the channel 0 is required
+     if ((flag_sample_ch0==1) &( flag_sample_ch1 == 0)){ //only the channel 0 is required
         AMux_Select(0);
         
         value_digit[count] = ADC_DelSig_Read32();
@@ -120,27 +127,30 @@ uint8 count=-1;
         if (count==Nsamples-1){ 
             for (int i=1;i<Nsamples;i++){
                 
-                value_digit[i]=ADC_DelSig_CountsTo_mVolts(value_digit[i]);
-                value_digit[i]=(value_digit[i]-offset_temp_mv)/coef_temp + offset_temp_c;
+                //value_digit[i]=ADC_DelSig_CountsTo_mVolts(value_digit[i]);
+                //value_digit[i]=(value_digit[i]-offset_temp_mv)/coef_temp + offset_temp_c;
                 if (value_digit[i]>65535) value_digit[i]=65535;
                 if (value_digit[i]<0) value_digit[i]=0; //limit the dynamic to positive values within 65535 for robustness
                 
-                sum += value_digit[i];
+                sum_0 += value_digit[i];
             }
-            sum= sum/Nsamples;
-            if (sum>65535) { //warn user of invalid value (see below)
-                sum=65535;
+            sum_0= sum_0/Nsamples;
+            if (sum_0>65535) { //warn user of invalid value (see below)
+                sum_0=65535;
                 flag_avgOVF=1;  
             }
 
         }
         
+        
+        
+        
         /*in order to use the same timer period both for 2 or 1 channels to be sampled we finish sampling 
         after 10ms here but we wait for 20ms to pass (1/50Hz) to update the register*/
-        if (count==2*Nsamples-1)  { 
-            slaveBuffer[3]=(sum & 0xFFFF) >>8;
-            slaveBuffer[4]= sum  & 0xFF;   
-            sum=0;        
+       if (count==2*Nsamples-1)  { 
+            slaveBuffer[3]=(sum_0 & 0xFFFF) >>8;
+            slaveBuffer[4]= sum_0  & 0xFF;   
+            sum_0=0;        
             count=-1;
         }    
         
@@ -162,8 +172,8 @@ uint8 count=-1;
         if (count==Nsamples-1){
             for (int i=1;i<Nsamples;i++){
                 
-                value_digit[i]=ADC_DelSig_CountsTo_Volts(value_digit[i]);
-                value_digit[i]=  offset_lux + (delta_lux)*(R*(vcc-value_digit[i])/vcc*(1-(vcc-value_digit[i])/vcc)-offset_ohm)/delta_ohm;
+                //value_digit[i]=ADC_DelSig_CountsTo_Volts(value_digit[i]);
+                //value_digit[i]=  offset_lux + (delta_lux)*(R*(vcc-value_digit[i])/vcc*(1-(vcc-value_digit[i])/vcc)-offset_ohm)/delta_ohm;
                 if (value_digit[i]>65535) value_digit[i]=65535;
                 if (value_digit[i]<0) value_digit[i]=0; //limit the dynamic to positive values within 65535 for robustness
                 
@@ -179,9 +189,11 @@ uint8 count=-1;
         
         /*in order to use the same timer period both for 2 or 1 channels to be sampled we finish sampling 
         after 10ms here but we wait for 20ms to pass (1/50Hz) to update the register*/
+        
+        
         if (count==2*Nsamples-1)  {  
             slaveBuffer[5]=(sum & 0xFFFF) >>8;
-            slaveBuffer[6]= sum  & 0xFF;   
+            slaveBuffer[6]= sum & 0xFF;   
             sum=0;        
             count=-1;
         }    
@@ -215,3 +227,13 @@ uint8 count=-1;
     }
 }
 /* [] END OF FILE */
+    
+    
+    
+    
+ 
+ // get "bits view" of test value
+
+
+    
+    
